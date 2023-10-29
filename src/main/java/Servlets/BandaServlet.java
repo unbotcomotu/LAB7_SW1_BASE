16b046402187e3b99aacf7a5145229ccf83d2233
alex-segovia <a20213849@pucp.edu.pe>
2023-10-29 07:57:29 -0500

    bandaServlet y listaBandas de nuevo

--- src/main/java/Servlets/BandaServlet.java
+++ src/main/java/Servlets/BandaServlet.java
@@ -1,20 +1,22 @@
 package Servlets;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
-
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "BandaServlet", value = "/BandaServlet")
+@WebServlet(name = "MainServlet",urlPatterns = {"/listaBandas"})
 public class BandaServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        RequestDispatcher view =request.getRequestDispatcher("listaBandas.jsp");
+        view.forward(request,response);
     }
 }
