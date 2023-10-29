1a8dc86501f1582d5215ed4903953cb3637277e2
alex-segovia <a20213849@pucp.edu.pe>
2023-10-29 07:59:41 -0500

    bandaServlet y listaBandas de nuevo xd

--- src/main/java/Servlets/BandaServlet.java
+++ src/main/java/Servlets/BandaServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "MainServlet",urlPatterns = {"/listaBandas"})
+@WebServlet(name = "BandaServlet",urlPatterns = {"/listaBandas"})
 public class BandaServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
